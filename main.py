21f1fe746c294ae2bebb5582ee4a208ed24094c9
ibrahimyandi <71317565+ibrahimyandi@users.noreply.github.com>
2021-03-07 15:25:12 +0300

    great power list created

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import discord
 from discord.ext import commands
-bot = commands.Bot(command_prefix="!ss ")
+from gp import greatPowers
+import re 
+
+bot = commands.Bot(command_prefix="!ss ", help_command=None)
 bot.remove_command('help')
+
 @bot.event
 async def on_ready():
     print("hazırım")
+    await bot.change_presence(activity=discord.Game(name='!ss help | git.io/JqkZM'))
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!ss'):
+        await bot.process_commands(message)
+        await message.delete() 
 
 @bot.command()
-async def statistics(ctx, arg):
-    await ctx.send(arg)
+async def gp(ctx, save, playerCount):
+    await ctx.send(greatPowers(save, playerCount))
 
 @bot.command()
-async def help(msg):
-    await msg.send("Command: !ss statistics [key] [id]")
+async def help(ctx):
+    await ctx.send("""```Command: !ss gp [save] [player count]\nDescription: Player shows great power ranking.```""")
 
 bot.run('ODE2NjIwNDUyODY4MjU5ODkw.YD9nEA.z-LnaEYWsvMeYDRwiUWQhbe2938')
