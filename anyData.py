6042858133b000f2ec7286fc5c40565118099646
ibrahimyandi <71317565+ibrahimyandi@users.noreply.github.com>
2021-05-15 14:54:12 +0300

    Update anyData.py

--- anyData.py
+++ anyData.py
@@ -2,7 +2,7 @@ import requests
 from operator import attrgetter
 
 def aData(save,dataId, playerCount):
-    response = requests.get('https://skanderbeg.pm/api.php?key=a6aeaf7782e8c5444b8e9f55cb5abc36&scope=getCountryData&save={0}&value=player;countryName;human;{1}&format=json'.format(save,dataId))
+    response = requests.get('https://skanderbeg.pm/api.php?key=a6aeaf7782e8c5444b8e9f55cb5abc36&scope=getCountryData&playersOnly=true&save={0}&value=player;countryName;human;{1}&format=json'.format(save,dataId))
     data = response.json()
     dataList = []
     class Players:
@@ -12,13 +12,24 @@ def aData(save,dataId, playerCount):
             self.value = value
     content = "```\n"
     for i in data:
-        if 'player' in data.get(i)[0] and '{0}'.format(dataId) in data.get(i)[0]:
+        try:
             if 'deving_stats' in dataId or 'total_mana_spent' in dataId:
-                dataList.append(Players(data.get(i)[0]['player'],data.get(i)[0]['countryName'], round(float(data.get(i)[0]['{0}'.format(dataId)]['s']))))
+                try:
+                    dataList.append(Players(data.get(i)[0]['player'],data.get(i)[0]['countryName'], round(float(data.get(i)[0]['{0}'.format(dataId)]['s']))))
+                except:
+                    dataList.append(Players("?",data.get(i)[0]['countryName'], round(float(data.get(i)[0]['{0}'.format(dataId)]['s']))))
             elif 'deving_stats' in dataId or 'deving_ratios' in dataId:
-                dataList.append(Players(data.get(i)[0]['player'],data.get(i)[0]['countryName'], data.get(i)[0]['{0}'.format(dataId)]['val'].replace("&lt;br&gt;","")))
+                try:
+                    dataList.append(Players(data.get(i)[0]['player'],data.get(i)[0]['countryName'], data.get(i)[0]['{0}'.format(dataId)]['val'].replace("&lt;br&gt;","")))
+                except:
+                    dataList.append(Players("?",data.get(i)[0]['countryName'], data.get(i)[0]['{0}'.format(dataId)]['val'].replace("&lt;br&gt;","")))
             else:
-                dataList.append(Players(data.get(i)[0]['player'],data.get(i)[0]['countryName'], round(float(data.get(i)[0]['{0}'.format(dataId)]))))
+                try:
+                    dataList.append(Players(data.get(i)[0]['player'],data.get(i)[0]['countryName'], round(float(data.get(i)[0]['{0}'.format(dataId)]))))
+                except:
+                    dataList.append(Players("?",data.get(i)[0]['countryName'], round(float(data.get(i)[0]['{0}'.format(dataId)]))))
+        except:
+            dataList.append(Players("?",data.get(i)[0]['countryName'], 0))
         dataList = sorted(dataList, key=attrgetter('value'), reverse=True)
     content += dataId.replace("_"," ").upper()
     for i in range(len(dataList[:int(playerCount)])):
