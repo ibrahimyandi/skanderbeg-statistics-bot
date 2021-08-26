55ec1cabd4e390faa0eeb61a5ca552098cde9e72
PacTi <71317565+ibrahimyandi@users.noreply.github.com>
2021-08-26 14:40:50 +0300

    Update score.py

--- score.py
+++ score.py
@@ -2,7 +2,7 @@ import requests
 from operator import attrgetter
 
 def scored(save):
-    response = requests.get('https://skanderbeg.pm/api.php?key=a6aeaf7782e8c5444b8e9f55cb5abc36&scope=getCountryData&save={0}&value=player;countryName;human;overall_strength;provinces;monthly_income;&format=json'.format(save))
+    response = requests.get('https://skanderbeg.pm/api.php?key=a6aeaf7782e8c5444b8e9f55cb5abc36&playersOnly=true&scope=getCountryData&save={0}&value=player;countryName;human;overall_strength;provinces;monthly_income;&format=json'.format(save))
     data = response.json()
     scoreList = []
     class Players:
@@ -19,22 +19,23 @@ def scored(save):
     content = content + "**SKORLAR**\n"
     countPlayers = 0
     for i in data:
-        if 'player' in data.get(i)[0]:
-            countPlayers += 1
-            if float(incomeAverage) < float(data.get(i)[0]['monthly_income']):
-                incomeAverage = float(data.get(i)[0]['monthly_income'])
-        if 'player' in data.get(i)[0]:
-            if float(oStrengthAverage) < float(data.get(i)[0]['overall_strength']):
-                oStrengthAverage = float(data.get(i)[0]['overall_strength'])
+        countPlayers += 1
+        if float(incomeAverage) < float(data.get(i)[0]['monthly_income']):
+            incomeAverage = float(data.get(i)[0]['monthly_income'])
+    
+        if float(oStrengthAverage) < float(data.get(i)[0]['overall_strength']):
+            oStrengthAverage = float(data.get(i)[0]['overall_strength'])
     incomeAverage = 30/countPlayers
     oStrengthAverage = 70/countPlayers
     for i in data:
-        if 'player' in data.get(i)[0] and 'monthly_income' in data.get(i)[0] and 'provinces' in data.get(i)[0]:
+        if 'monthly_income' in data.get(i)[0] and 'provinces' in data.get(i)[0]:
             if float(data.get(i)[0]['provinces']) >= 1:
-                scoreList.append(Players(data.get(i)[0]['player'],data.get(i)[0]['countryName'],float(data.get(i)[0]['monthly_income']),float(data.get(i)[0]['overall_strength']),0))
+                try:
+                    scoreList.append(Players(data.get(i)[0]['player'],data.get(i)[0]['countryName'],float(data.get(i)[0]['monthly_income']),float(data.get(i)[0]['overall_strength']),0))
+                except:
+                    scoreList.append(Players("?",data.get(i)[0]['countryName'],float(data.get(i)[0]['monthly_income']),float(data.get(i)[0]['overall_strength']),0))
     scoreList = sorted(scoreList, key=attrgetter('oStrength'), reverse=True)
     for i in range(len(scoreList)):
-        print(scoreList[i].country,scoreList[i].oStrength)
         scoreList[i] = Players(scoreList[i].country, scoreList[i].player, float(scoreList[i].income), float((len(scoreList)-i)*(70/len(scoreList))),0)
     scoreList = sorted(scoreList, key=attrgetter('income'), reverse=True)
     for i in range(len(scoreList)):
